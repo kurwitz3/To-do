5a1d62f57bf3ff980fece592f075488c8371b6eb
kurwitz3 <kurwitz3@aol.com>
2021-07-01 00:08:33 -0400

    added event litener to delete buttons on li's

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const workButton = document.querySelector('button')
+const workInput = document.querySelector('input')
+const workOl = document.getElementById('work-ol')
 
 document.addEventListener('DOMContentLoaded',function(){
-  addWorkTask()
+  
 })
 
+function appendDeleteButton(element){
+    const completeButton = document.createElement('button')
+    completeButton.setAttribute('class','work-complete-button')
+    completeButton.innerText = 'Complete Task'
+    element.appendChild(completeButton)
+}
 
-function addWorkTask(){
-    const workInput = document.querySelector('input')
-    const workOl = document.getElementById('work-ol')
-    
-  workButton.addEventListener('click',() =>{
+function createLi(){
     let workLi = document.createElement('li')
     workLi.setAttribute('class','work-li')
-    const completeButton = document.createElement('button')
-    completeButton.innerText = 'Completed Task'
     workLi.innerText = workInput.value
-    workOl.append(workLi,completeButton)
+    workOl.append(workLi)
+    appendDeleteButton(workLi)
     workInput.value = ''
-})
- 
 }
+
+ workButton.addEventListener('click',createLi)
+ workOl.addEventListener('click',function(e){
+    e.target.closest('li').remove()
+ })
+ 
