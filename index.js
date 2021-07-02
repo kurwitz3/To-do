2cc3dd31434c7b5ea4cf5d187ebbca707f3ca3b7
kurwitz3 <kurwitz3@aol.com>
2021-07-01 20:47:14 -0400

    got the create button functionality to work

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const workButton = document.querySelector('button')
-const workInput = document.querySelector('input')
-const workOl = document.getElementById('work-ol')
+const workButton = document.getElementsByClassName('button')
+const workInput = document.getElementsByClassName('input')
+const workOl = document.getElementsByClassName('work-ol')
 
 document.addEventListener('DOMContentLoaded',function(){
-  
+butttonListener()
+deleteButton( )
 })
 
 function appendDeleteButton(element){
@@ -13,17 +14,30 @@ function appendDeleteButton(element){
     element.appendChild(completeButton)
 }
 
-function createLi(){
+function createLi(input,ol){
     let workLi = document.createElement('li')
     workLi.setAttribute('class','work-li')
-    workLi.innerText = workInput.value
-    workOl.append(workLi)
+    workLi.innerText = input
+    ol.append(workLi)
     appendDeleteButton(workLi)
-    workInput.value = ''
+    
 }
 
- workButton.addEventListener('click',createLi)
- workOl.addEventListener('click',function(e){
-    e.target.closest('li').remove()
- })
+function butttonListener(){
+  for(i = 0; i < workButton.length; i++)
+    workButton[i].addEventListener('click', (e) => {
+      e.preventDefault()
+      let inputValue = e.target.form.elements[0].value
+      let div = e.target.form.nextElementSibling
+      let taskOl = div.lastElementChild
+      createLi(inputValue,taskOl)
+      e.target.form.elements[0].value = ""
+    })
+}
  
+function deleteButton(){
+  for(i = 0; i< workOl.length; i++)
+    workOl[i].addEventListener('click',function(e){
+      e.target.closest('li').remove()
+    })
+}
