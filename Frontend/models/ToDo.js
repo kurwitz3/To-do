fd6835cfcb90170b887d8a8b987d1bc213b44ec6
kurwitz3 <kurwitz3@aol.com>
2021-07-06 22:49:07 -0400

    touch ups

--- Frontend/models/ToDo.js
+++ Frontend/models/ToDo.js
@@ -7,41 +7,34 @@ class ToDo{
         ToDo.createLi(this.listItem)
     }
     
-    static appendDeleteButton(element){
-      let todoId = ToDo.allToDos[this.allToDos.length -1].id
-      const completeButton = document.createElement('button')
-      completeButton.setAttribute('class','work-complete-button')
-      completeButton.setAttribute('id',`${todoId}`)
-      completeButton.innerText = 'Task Completed'
-      element.appendChild(completeButton)
-      completeButton.addEventListener('click',() => {
-        API.deleteTodo(todoId)
-        element.remove()
-      })
-    }
-    
-    static createLi(input){
-      let workLi = document.createElement('li')
-      workLi.setAttribute('class','work-li')
-      workLi.innerText = input
-      workOl.append(workLi)
-      ToDo.appendDeleteButton(workLi)
-    }
-    
-     static butttonListener(){
-      const workButton = document.getElementById('button')
-        workButton.addEventListener('click', (e) => {
-          const workInput = document.getElementById('input').value
-          e.preventDefault()
-          API.createToDo(workInput)
-          e.target.form.elements[0].value = ""
-        })
-    }
-
+  static appendDeleteButton(element){
+    let todoId = ToDo.allToDos[this.allToDos.length -1].id
+    const completeButton = document.createElement('button')
+    completeButton.setAttribute('class','work-complete-button')
+    completeButton.setAttribute('id',`${todoId}`)
+    completeButton.innerText = 'Task Completed'
+    element.appendChild(completeButton)
+    completeButton.addEventListener('click',() => {
+      API.deleteTodo(todoId)
+      element.remove()
+    })
+  }
   
-
-
+  static createLi(input){
+    let workLi = document.createElement('li')
+    workLi.setAttribute('class','work-li')
+    workLi.innerText = input
+    workOl.append(workLi)
+    ToDo.appendDeleteButton(workLi)
+  }
   
-
-
+  static butttonListener(){
+   const workButton = document.getElementById('button')
+     workButton.addEventListener('click', (e) => {
+       const workInput = document.getElementById('input').value
+       e.preventDefault()
+       API.createToDo(workInput)
+       e.target.form.elements[0].value = ""
+     })
+  }
 }
